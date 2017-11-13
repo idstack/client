a3ef94bd2c412008a9919ae368dd8b99b6f9bc0c
chaminw <chamin.13@cse.mrt.ac.lk>
2017-11-13 11:00:27 +0530

    Add protractor conf file

---
+++ test/protractor.conf.js
@@ -0,0 +1,8 @@
+exports.config = {
+    specs: ['*-spec.js'],
+    capabilities: {
+        browserName: 'chrome'
+    },
+    baseUrl: 'http://localhost:63342/IDStack-Client-v2',
+    framework: 'jasmine'
+};
